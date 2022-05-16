c367e5d3262f1c2026509ca98af9d2ac3475db57
RRRLAI <104291622+RRRLAI@users.noreply.github.com>
2022-05-16 21:31:30 +0800

    彈珠軌跡

---
+++ Assets/Scripts/LearnData.cs
@@ -0,0 +1,14 @@
+//藍色 保留字
+//白色 名稱
+//綠色 資料類型
+using UnityEngine;
+
+public class LearnData : MonoBehaviour
+{
+    int hp;
+
+    int lv;
+    float exp;
+    string playerName;
+    bool hasCureSkill;
+}
